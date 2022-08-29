c9de9f805ba1909fd0acbbd12d7e5d0d7ec35842
jessehzx <jessehuangzx@gmail.com>
2022-08-29 10:52:43 +0800

    init commit

---
+++ src/main/java/com/jessehzx/springbootrestfulscaffold/SpringbootRestfulScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.jessehzx.springbootrestfulscaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulScaffoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jessehzx/springbootrestfulscaffold/SpringbootRestfulScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessehzx.springbootrestfulscaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulScaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
