297e29b6cdf734cf65264d8a1a22d33cf52c02a1
jessehzx <jessehuangzx@gmail.com>
2022-09-01 21:52:48 +0800

    rename springboot startup class

--- src/test/java/com/jessehzx/springbootrestfulscaffold/ApplicationTests.java
+++ src/test/java/com/jessehzx/springbootrestfulscaffold/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootRestfulScaffoldApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
